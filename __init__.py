7edbf85fe56d076f4dfc3a0e3d479dd218b4addc
Josefiino <74570753+Josefiino@users.noreply.github.com>
2021-03-25 12:35:01 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,46 +1,41 @@
 from aqt import mw
-from aqt import browser
 from anki.hooks import addHook
-# import the "show info" tool from utils.py
-from aqt.utils import showInfo
-# import aqt.utils
-# import all of the Qt GUI library
 from aqt.qt import *
-import requests
-import csv
-import re
-
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
 
 config = mw.addonManager.getConfig(__name__)
 # tag to add
 tag_to_add = config['tag_to_add']
-# field to change
-note_tags = config['note_tags']
+tag_to_add_2 = config['tag_to_add_2']
 # shortcuts
 shortcut_tags = config['shortcut_tags']
+shortcut_tags_2 = config['shortcut_tags_2']
 
-
-def show_selected_from_browser(nids):
+def show_selected_from_browser(nids, tag):
     for id in nids:
         note = mw.col.getNote(id)
-        note.addTag(tag_to_add)
+        note.addTag(tag)
         note.flush()
         mw.reset()
 
-
-# create a new menu item, "Add Tag"
+# create a new menu items, "Add Tag"
 def setupMenu(browser):
     action = QAction(f'Add "{tag_to_add}" tag', browser)
     action.setShortcut(shortcut_tags)
     # set it to call testFunction when it's clicked
-    action.triggered.connect(lambda: onRegenerate(browser))
+    action.triggered.connect(lambda: onRegenerate(browser, tag_to_add))
     # and add it to the tools menu
     browser.form.menuEdit.addAction(action)
 
-def onRegenerate(browser):
-    show_selected_from_browser(browser.selectedNotes())
+    action2 = QAction(f'Add "{tag_to_add_2}" tag', browser)
+    action2.setShortcut(shortcut_tags_2)
+    # set it to call testFunction when it's clicked
+    action2.triggered.connect(lambda: onRegenerate(browser, tag_to_add_2))
+    # and add it to the tools menu
+    browser.form.menuEdit.addAction(action2)
+
+
+def onRegenerate(browser, tags):
+    show_selected_from_browser(browser.selectedNotes(), tags)
 
 addHook("browser.setupMenus", setupMenu)
 
